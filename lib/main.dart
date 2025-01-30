8a85c1385417fa585e4eef9efa9ad68534c61189
usman619 <usmanshahab191@gmail.com>
2025-01-30 23:26:44 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/pages/home_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: const ColorScheme.dark(),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
